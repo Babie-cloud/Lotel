e63486221f559bd2ce21480abed6d436c7c28276
Babie-cloud <karliegionacubahiro@gmail.com>
2026-07-22 11:25:43 +0200

    debug

--- ng-hotel/src/app/app.routes.server.ts
+++ ng-hotel/src/app/app.routes.server.ts
@@ -2,7 +2,15 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'userdashboard',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'userdashboard/**',
+    renderMode: RenderMode.Client,
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
 ];
